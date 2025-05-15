6a555beda19fd5a2bde2ae5f7e49a72f5e8931ce
alex <aadami@students.cs.ubc.ca>
2025-05-15 16:33:20 -0700

    confusionMatrix function follows standard format

--- code/model.py
+++ code/model.py
@@ -1,6 +1,7 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 import pandas as pd
+import numpy as np
 
 
 # Load the dataset
@@ -18,19 +19,29 @@ X = scaler.fit_transform(X)
 
 # Split the dataset into training and testing sets
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-for k in range(1, 60, 20):
+def confusionMatrix(y_test, y_pred):
+    """
+    Returns confusion matrix accoring to standard format
+    """
+    cm = confusion_matrix(y_test, y_pred)
+    TP = cm[1][1]
+    TN = cm[0][0]
+    FP = cm[0][1]
+    FN = cm[1][0]
+    return np.array([[TP, FN],
+                     [FP, TN]])
+
+for k in [1,20,40,60]:
     classifier = KNeighborsClassifier(n_neighbors=k,weights='distance')
 
-    # Fit the classifier to the training data
+    # Fit the classifier to the training data and make predictions
     classifier.fit(X_train, y_train)
-    # Make predictions on the test data
     y_pred = classifier.predict(X_test)
     # Evaluate the classifier
     print(f"k={k}")
     print(f"Accuracy = {classifier.score(X_test, y_test)}")
-    print(confusion_matrix(y_test, y_pred))
+    print(confusionMatrix(y_test, y_pred))
     print(classification_report(y_test, y_pred))
     print('-' * 40)
-
